f040ae22a2af602c8ffb7450ec13a8360ac0899b
Longchengg <13452969830@163.com>
2020-06-08 21:02:24 +0800

    home修改

--- Supervision/Class/LookBroad/Models/LookBroadViewModel.m
+++ Supervision/Class/LookBroad/Models/LookBroadViewModel.m
@@ -10,27 +10,4 @@
 
 @implementation LookBroadViewModel
 
-
-
-//alertCount = 3;
-//       bidEnoughAlertMessage = "\U60a8\U7684\U673a\U7535\U914d\U5957\U62db\U6807\U4eba\U5458\U4e0d\U8db3\Uff01\U8bf7\U53ca\U65f6\U5904\U7406\Uff01";
-//       calock =         {
-//           companyId = 27;
-//           companyName = "<null>";
-//           id = 14;
-//           limitDay = 0;
-//           type = "CA\U9501";
-//           validTime = "2019-12-27";
-//       };
-//       comSafetyLicenseDTO =         {
-//           alertStatus =             {
-//               desc = "\U6b63\U5e38";
-//               status = 0;
-//           };
-//           backStatus = 0;
-//           backStatusStr = "\U7a7a\U95f2";
-//           companyId = 27;
-//           companyName = "\U91cd\U5e86\U67d0\U76d1\U7406\U6709\U9650\U516c\U53f8";
-//           id = 16;
-//           image = "https://jbzl-saas-test.oss-cn-shanghai.aliyuncs.com//license_image/2806295b2c2a4dbe9b0a7f992b
 @end
